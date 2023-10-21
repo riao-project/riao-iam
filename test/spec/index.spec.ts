52a18b92c08ef7a720663cc300d41b02764a73b4
Drew Immerman <drewimmerman@gmail.com>
2023-10-21 13:11:13 -0400

    Setup riao & test database

--- test/spec/index.spec.ts
+++ test/spec/index.spec.ts
@@ -1,5 +1,13 @@
 import 'jasmine';
 import * as index from '../../src';
+import { maindb } from '../../database/main';
+
+beforeAll(async () => {
+	await maindb.init();
+});
+afterAll(async () => {
+	await maindb.disconnect();
+});
 
 describe('riao-iam', () => {
 	it('exports a', () => {
