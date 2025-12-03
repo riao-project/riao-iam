e41b1f957ed2351dd624d86bdd623893f79e0593
Drew Immerman <drewimmerman@gmail.com>
2025-12-03 06:21:31 -0500

    clean: lint errors

--- test/spec/index.spec.ts
+++ test/spec/index.spec.ts
@@ -1,11 +1,8 @@
 import 'jasmine';
-import { clearDatabases } from '../database';
 import { maindb } from '../../database/main';
 
 beforeAll(async () => {
-	console.log('Initializing main test database...');
 	await maindb.init();
-	console.log('Main test database initialized.');
 	//await clearDatabases();
 	//await testdb.init();
 });
